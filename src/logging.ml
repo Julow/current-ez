2461a76410261941a5d4fbaa9db558129d95e275
Jules Aguillon <juloo.dsi@gmail.com>
2019-09-28 02:07:51 +0200

    Logging cli

--- src/logging.ml
+++ src/logging.ml
@@ -1,4 +1,7 @@
-let init ?(level = Logs.Info) () =
-  Fmt_tty.setup_std_outputs ();
-  Logs.set_level (Some level);
+let init style_renderer level =
+  Fmt_tty.setup_std_outputs ?style_renderer ();
+  Logs.set_level level;
   Logs.set_reporter (Logs_fmt.reporter ())
+
+let cli =
+  Cmdliner.Term.(const init $ Fmt_cli.style_renderer () $ Logs_cli.level ())
--- src/main.ml
+++ src/main.ml
@@ -2,7 +2,7 @@
 
 let desc = Pipeline.{ ocamlformat = Ocamlformat.Dune }
 
-let main config mode repo =
+let main config mode repo () =
   let repo = match repo with Some d -> d | None -> Sys.getcwd () in
   let repo = Current_git.Local.v (Fpath.v repo) in
   let engine =
@@ -20,10 +20,9 @@ let repo =
     value & pos 0 (some dir) None & info ~doc:"The repository" ~docv:"DIR" [])
 
 let () =
-  Logging.init ();
   let open Term in
   exit @@ eval
   @@
   let doc = "Easy ci" in
-  ( const main $ Current.Config.cmdliner $ Current_web.cmdliner $ repo,
+  ( const main $ Current.Config.cmdliner $ Current_web.cmdliner $ repo $ Logging.cli,
     info "current-ez" ~doc )
